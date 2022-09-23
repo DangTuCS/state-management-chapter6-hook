687d05e3a1261cfeb66efab04a4a0c82c58a9d93
DangTuCS <dangtu12350@gmail.com>
2022-09-23 10:32:42 +0700

    4th-commit-useListenable

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
+import 'dart:async';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 extension CompactMap<T> on Iterable<T?> {
@@ -12,7 +12,6 @@ extension CompactMap<T> on Iterable<T?> {
 void testId() {
   final values = [1, 2, null, 3];
   final nonNullValues = values.compactMap((_) => 'Hello');
-  print(nonNullValues);
 }
 
 void main() {
@@ -35,31 +34,42 @@ class MyApp extends StatelessWidget {
   }
 }
 
-const url =
-    'https://cdn.pixabay.com/photo/2022/08/14/21/47/snake-7386684__340.jpg';
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+
+  CountDown({required int from}) : super(from) {
+    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
+        .takeWhile((element) => value >= 1)
+        .listen((event) {
+      value = event;
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    sub.cancel();
+  }
+}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final future = useMemoized(() =>
-      NetworkAssetBundle(Uri.parse(url))
-          .load(url)
-          .then((value) => value.buffer.asUint8List())
-          .then((value) => Image.memory(value)),
-    );
-    print('running');
-    final snapshot = useFuture(future);
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hooks demo'),
       ),
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            snapshot.data
-          ].compactMap().toList(),
+            Text(notifier.value.toString()),
+          ],
         ),
       ),
     );
