4f26ed58636f7cfcfc2d8423f75b80e6e5544214
DangTuCS <dangtu12350@gmail.com>
2022-09-23 09:56:08 +0700

    3rd-commit-useFuture-useMemoized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,19 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
+      map(transform ?? (e) => e).where((e) => e != null).cast();
+}
+
+void testId() {
+  final values = [1, 2, null, 3];
+  final nonNullValues = values.compactMap((_) => 'Hello');
+  print(nonNullValues);
+}
 
 void main() {
   runApp(const MyApp());
@@ -21,32 +35,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
+const url =
+    'https://cdn.pixabay.com/photo/2022/08/14/21/47/snake-7386684__340.jpg';
+
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    var text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-      return null;
-    }, [controller]);
-
+    final future = useMemoized(() =>
+      NetworkAssetBundle(Uri.parse(url))
+          .load(url)
+          .then((value) => value.buffer.asUint8List())
+          .then((value) => Image.memory(value)),
+    );
+    print('running');
+    final snapshot = useFuture(future);
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: const Text('Hooks demo'),
       ),
       body: SafeArea(
         child: Column(
           children: [
-            TextFormField(
-              controller: controller,
-            ),
-            Text('You typed: ${text.value}'),
-          ],
+            snapshot.data
+          ].compactMap().toList(),
         ),
       ),
     );
