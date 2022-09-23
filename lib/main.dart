0cc339cb660cdcc4126e2624ae0cb1945e7de257
DangTuCS <dangtu12350@gmail.com>
2022-09-23 10:59:41 +0700

    5th-commit-useAnimationController-useScrollController

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,8 @@
-import 'dart:async';
 import 'dart:core';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([E? Function(T?)? transform]) =>
-      map(transform ?? (e) => e).where((e) => e != null).cast();
-}
-
-void testId() {
-  final values = [1, 2, null, 3];
-  final nonNullValues = values.compactMap((_) => 'Hello');
-}
 
 void main() {
   runApp(const MyApp());
@@ -34,41 +24,83 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-
-  CountDown({required int from}) : super(from) {
-    sub = Stream.periodic(const Duration(seconds: 1), (v) => from - v)
-        .takeWhile((element) => value >= 1)
-        .listen((event) {
-      value = event;
-    });
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    super.dispose();
-    sub.cancel();
-  }
+extension Normalize on num {
+  num normalized(
+    num selfRangeMin,
+    num selfRangeMax, [
+    num normalizedRangeMin = 0.0,
+    num normalizedRangeMax = 1.0,
+  ]) =>
+      (normalizedRangeMax - normalizedRangeMin) *
+          ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) +
+      normalizedRangeMin;
 }
+
+const imageUrl =
+    'https://cdn.pixabay.com/photo/2016/11/06/19/42/cat-1803904__340.jpg';
+const imageHeight = 300.0;
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized(() => CountDown(from: 20));
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      upperBound: 1.0,
+      lowerBound: 0.0,
+    );
+
+    final size = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1.0,
+      upperBound: 1.0,
+      lowerBound: 0.0,
+    );
+
+    final controller = useScrollController();
+    useEffect(() {
+      controller.addListener(() {
+        final newOpacity = max(imageHeight - controller.offset, 0.0);
+        final normalized = newOpacity.normalized(0.0, imageHeight).toDouble();
+        opacity.value = normalized;
+        size.value = normalized;
+      });
+      return null;
+    }, [controller]);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Hooks demo'),
       ),
       body: SafeArea(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(notifier.value.toString()),
+            SizeTransition(
+              sizeFactor: size,
+              axis: Axis.vertical,
+              axisAlignment: -1.0,
+              child: FadeTransition(
+                opacity: opacity,
+                child: Image.network(
+                  imageUrl,
+                  height: imageHeight,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                controller: controller,
+                itemCount: 100,
+                itemBuilder: (context, index) => ListTile(
+                  title: Text(
+                    'Person $index',
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
